d999efdd53f99e8ef0779e957c0e8da3d8771a2d
gjurado1299 <gjurado1299@gmail.com>
2021-01-07 13:37:08 +0100

    Mision 1

---
+++ Comienzo isla/Assets/Scripts/Oleadas/Timer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float timerValue;
+
+    private bool runTimer = false;
+
+    public UnityEvent TimerEvent;
+
+    public void StartTimer(float _timerValue)
+    {
+        timerValue = _timerValue;
+        runTimer = true;
+    }
+
+    public void StopTimer()
+    {
+        runTimer = false;
+
+    }
+
+    private void Update()
+    {
+        if (runTimer)
+        {
+            timerValue -= Time.deltaTime;
+
+            if (timerValue <= 0f)
+            {
+                StopTimer();
+
+                if (TimerEvent != null)
+                {
+                    TimerEvent.Invoke();
+                }
+            }
+        }
+    }
+
+}
---
+++ Comienzo isla/Assets/Scripts/Oleadas/WaveManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Timer))]
+public class WaveManager : MonoBehaviour
+{
+    private int currentWave = 0;
+    private int enemiesInWave;
+    private int enemiestoSpawn;
+
+    private Timer timer;
+
+    private List<Transform> spawners = new List<Transform>();
+
+    public float spawnDelay;
+
+    public float roundDelay;
+
+    public int enemyIncreaseFactor;
+
+    public GameObject enemy;
+
+
+    private void Start()
+    {
+        timer = GetComponent<Timer>();
+
+        // Actualizar mision info
+
+        foreach(Transform child in transform)
+        {
+            spawners.Add(child);
+        }
+    }
+
+    public void StartWave()
+    {
+        // Si havook ha muerto, parar
+        Debug.Log("EMPEZANDO");
+        currentWave += 1;
+        Debug.Log("RONDA "+ currentWave.ToString());
+        enemiesInWave = enemyIncreaseFactor * currentWave;
+        enemiestoSpawn = enemiesInWave;
+
+        Debug.Log("ENEMIGOS "+ enemiesInWave.ToString());
+        Debug.Log("ENEMIGOS TOTALES "+ enemiestoSpawn.ToString());
+        // Actualizar mision info
+
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        for (int i = 0; i < enemiestoSpawn; i++)
+        {
+            // Si havook ha muerto, parar
+
+            Instantiate(enemy, GetRandomSpawner(), Quaternion.identity);
+
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    public void EndWave()
+    {
+        // Actualizar mision info
+
+        timer.StartTimer(roundDelay);
+    }
+
+    public void EnemyDied()
+    {
+        enemiesInWave -= 1;
+
+        // Actualizar mision info
+        // ui.UpdateUI(currentWave, enemiesInWave);
+
+        if (enemiesInWave == 0)
+        {
+            EndWave();
+        }
+    }
+
+    public Vector3 GetRandomSpawner()
+    {
+        return spawners[Random.Range(0, spawners.Count)].position;
+    }
+}
